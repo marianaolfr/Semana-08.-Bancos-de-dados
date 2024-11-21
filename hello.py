419c85bf6f436402bf343cee569a3f85f2c21aaa
mariana f. <91221958+marianaolfr@users.noreply.github.com>
2024-11-20 23:38:14 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
 
-basedir = os.path.abspath(os.path.dirname(__file__)) 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'top_secret_key_mariana'
@@ -21,24 +21,46 @@ moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    known = False
+    users = User.query.all()
+
     if form.validate_on_submit():
-        name = form.name.data
-        user = User.query.filter_by(name=name).first()
+        user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(name=name)
+            role = Role.query.filter_by(name="User").first()
+            if role is None:
+                role = Role(name="User")
+                db.session.add(role)
+                db.session.commit()
+            user = User(username=form.name.data, role=role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -46,17 +68,25 @@ def index():
         else:
             session['known'] = True
             flash('Happy to see you again!')
-        session['name'] = name
+        session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), users=users, current_time=datetime.utcnow())
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return dict(db=db, User=User, Role=Role)
 
-if __name__ == '_main_':
+
+if __name__ == '__main__':
     app.run(debug=True)
